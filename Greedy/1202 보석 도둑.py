8b6ffc2afa28629ab4c8fa213fbb30e16ee9d92f
think-virus <chuncccc1004@gmail.com>
2022-02-09 23:09:26 +0900

    #1202 보석 도둑 -> 코드 공부 완료

--- Greedy/1202 보석 도둑.py
+++ Greedy/1202 보석 도둑.py
@@ -1,28 +1,27 @@
-# 가방이 허락하는 무게에 최대한의 가격의 보석을 넣어야 함
+# 내가 잘못 생각했던 부분은 jewel에 넣은 항목을 heaqp.heappop으로 빼오면서 삭제하면 비교가 안될 거라고 생가했는데 아래처럼 구현하면 됨
 import sys
 import heapq
 
-N, K = map(int,sys.stdin.readline().rstrip().split())
-zem_list = []
-bag_list = []
-price_sum = 0
+input = sys.stdin.readline
 
-for _ in range(N) :
-    zem_list.append(list(map(int,sys.stdin.readline().rstrip().split()))) # 보석 정보 리스트에 저장 0 : 무게 / 1 : 가격
-zem_list.sort(key=lambda x:x[1], reverse=True) # 가격 높은 순으로 정렬
+N, K = map(int, input().split())
+jewel = []
+bag = []
+for _ in range(N):
+    heapq.heappush(jewel, list(map(int, input().split())))
+for _ in range(K):
+    bag.append(int(input()))
 
-for _ in range(K) :
-    heapq.heappush(bag_list,int(sys.stdin.readline().rstrip()))
+bag.sort() # 가방의 크기가 작은 순으로 정렬
 
-while bag_list :
-    bag = heapq.heappop(bag_list)
-    idx = 0
-    for M, V in zem_list:
-        if M <= bag:  # 가방 안에 들어갈 수 있는 무게
-            price_sum += V
-            zem_list = zem_list[idx+1:]
-            break
-        idx += 1
+result = 0
+candidate = []
+for b in bag:
+    while jewel and b >= jewel[0][0]: # 작은 순으로 하고 그냥 넣어버리는 이유가 어차피 다음에 대입될 가방의 크기는 현재보다 크기 때문에 보석을 넣을 수 있음
+        w, v = heapq.heappop(jewel)
+        heapq.heappush(candidate, -v) # 최대 힙을 구현하기 위해 -를 붙임
 
-print(price_sum)
+    if candidate:
+        result -= heapq.heappop(candidate) # 들어갈 수 있는 보석 중 가장 큰 값을 가져옴
 
+print(result)
