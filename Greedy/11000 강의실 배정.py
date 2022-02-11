dc63ccbfa105047c1059c5929b5c3d347726fdf9
think-virus <chuncccc1004@gmail.com>
2022-02-11 21:57:37 +0900

    #11000 강의실 배정 -> 해결 알고리즘 확인 후, 코드 작성 및 이해 완료

--- Greedy/11000 강의실 배정.py
+++ Greedy/11000 강의실 배정.py
@@ -4,42 +4,39 @@
 
 # 처음 수업이 끝나고 바로 다음으로 오는 수업 찾기
 
-"""
-8
-1 3
-1 5
-2 4
-2 5
-3 4
-3 5
-5 9
-7 8
-"""
+# N이 최대 20만이기 때문에 O(n^2)의 시간 복잡도로는 무조건 time over 걸림
+# -> 정렬을 잘 사용해서 해결 필요
+
+# 해결 알고리즘 확인 : https://hongcoding.tistory.com/79
+# 1. 현재 회의실에서의 회의가 끝나는 시간보다 다음 회의의 시작시간이 빠르면 회의실을 하나 추가로 개설한다.
+# 2. 현재 회의실에서 회의가 끝나는 시간보다 다음 회의의 시작시간이 느리면 현재 존재하는 회의실에서 이어서 회의 개최가 가능하다.
 
 import sys
 import heapq
 
 N = int(sys.stdin.readline().rstrip())
 class_list = []
-class_room = 0
+class_room = []
 
 for _ in range(N) :
-    heapq.heappush(class_list,tuple(map(int,sys.stdin.readline().rstrip().split())))
+    class_list.append(tuple(map(int,sys.stdin.readline().rstrip().split()))) # 회의의 [시작, 끝] 시간을 세트로 하나의 리스트에 모두 입력
 
-while len(class_list) - 1 :
-    class_room += 1
-    spare_list = []
-    start_class = heapq.heappop(class_list)
-    for _ in range(len(class_list)) :
-        _class = heapq.heappop(class_list)
-        if start_class[1] > _class[0] :
-            heapq.heappush(spare_list,_class)
-        else :
-            start_class = _class
-    if spare_list :
-        class_list = spare_list
+class_list.sort() # 시작시간을 기준으로 정렬
 
-if class_list :
-    class_room += 1
+heapq.heappush(class_room,class_list[0][1]) # 첫 회의의 종료시간을 새로운 큐에 넣어줌
 
-print(class_room)
+# 이제 2번째 회의부터 첫 회의와 비교
+# 두 번째 회의의 시작 시간이 첫 회의의 종료시간보다 빠르면 새로 회의를 개최
+# 아니면 기존 회실 확인
+
+# 아래 코드는 room에서 한 번 pop을 해주고 새로운 회의 시간을 push하는 것 -> 새로 회의를 개설해야 하면, room에서 개로운 회의의 종료시간을 push해주면 됨
+# 우선순위 큐를 쓰는 이유? -> 종료시간이 빠른 회의부터 다음 회의를 이어서 개최해야 하기 때문
+for _class in class_list[1:] :
+    end_time = heapq.heappop(class_room)
+    if end_time > _class[0] : # 새로운 회의 개설
+        heapq.heappush(class_room,_class[1])
+    else :
+        end_time = _class[1]
+    heapq.heappush(class_room,end_time)
+
+print(len(class_room))
