d2b71ede9f2d53f16318efc9d3df6230e88c915e
think-virus <chuncccc1004@gmail.com>
2022-02-11 12:40:22 +0900

    #11000 강의실 배정 -> 시간초과 (heapq 사용)

--- Greedy/11000 강의실 배정.py
+++ Greedy/11000 강의실 배정.py
@@ -4,26 +4,42 @@
 
 # 처음 수업이 끝나고 바로 다음으로 오는 수업 찾기
 
+"""
+8
+1 3
+1 5
+2 4
+2 5
+3 4
+3 5
+5 9
+7 8
+"""
+
 import sys
+import heapq
+
 N = int(sys.stdin.readline().rstrip())
 class_list = []
 class_room = 0
 
 for _ in range(N) :
-    class_list.append(tuple(map(int,sys.stdin.readline().rstrip().split())))
+    heapq.heappush(class_list,tuple(map(int,sys.stdin.readline().rstrip().split())))
 
-class_list.sort(key= lambda x:(x[0],x[1]))
-
-while len(class_list) - 1 > 0  :
+while len(class_list) - 1 :
     class_room += 1
-    class_0 = class_list[0]
-    class_list = class_list[1:]
-    for _class in class_list :
-        if class_0[1] <= _class[0] : # 0의 위치의 수업의 끝보다 0다음의 수업의 시작이 큰 경우
-            class_list.remove(_class)
-            class_0 = _class
+    spare_list = []
+    start_class = heapq.heappop(class_list)
+    for _ in range(len(class_list)) :
+        _class = heapq.heappop(class_list)
+        if start_class[1] > _class[0] :
+            heapq.heappush(spare_list,_class)
+        else :
+            start_class = _class
+    if spare_list :
+        class_list = spare_list
 
-if len(class_list) :
+if class_list :
     class_room += 1
 
 print(class_room)
