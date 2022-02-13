6c2eda32cb827779e66eed9b3eae429f9147c0c6
think-virus <chuncccc1004@gmail.com>
2022-02-13 18:39:22 +0900

    #1700 멀티탭 스케줄링

--- Greedy/1700 멀티탭 스케줄링.py
+++ Greedy/1700 멀티탭 스케줄링.py
@@ -1,37 +1,36 @@
 # 1. 이후에 사용할 콘센트는 최대한 그대로 놓는 게 좋음
 # 2. 처음에 꽂혀 있어야 하는 항목들은 바로 분류
 # 3. 꽂혀있는 항목들의 count 실행
-from collections import deque
 
 """
 2 7
 2 3 2 3 1 2 7
 """
-
+from collections import deque
 import sys
+
 N, K = map(int, sys.stdin.readline().split())  # ex) 2 7
 Result = 0
 
-def Check_common(seq1,seq2):
-    diff = list(seq1-seq2) # 다시 사용하지 않는 제품 확인
-    if diff :
-        return diff[0]
-    else :
-        return False
+Order_list = deque(map(int,sys.stdin.readline().split()))
+Power_socket_list = []
+while not len(Power_socket_list) - N == 0 :
+    Product = Order_list.popleft()
+    if Product in Power_socket_list :
+        continue
+    Power_socket_list.append(Product)
 
-Order_list = list(map(int,sys.stdin.readline().split()))
-Power_socket_list = Order_list[:N]
-Order_list = deque(Order_list[N:]) # deque 자료형으로 변환
 
-for _ in range(K-N):
+for _ in range(len(Order_list)):
     Product = Order_list.popleft()
 
     if Product in Power_socket_list : # 이미 꽂혀있는 경우
         continue
     else : # 다시 꽂힐 일이 없는 프로덕트 확인
-        tmp = Check_common(set(Power_socket_list), {Product}|set(Order_list))
-        if tmp : # 다시 꽂힐 일이 없는 제품이 있을 경우 (제품 번호는 0은 없기 때문에 그냥 이렇게 적어도 괜찮음)
-            Power_socket_list.remove(tmp) # 이미 꽂혀있는 제품 중 가장 마지막에 사용하는 제품 제거
+        diff = list(set(Power_socket_list).difference({Product}|set(Order_list)))  # 다시 사용하지 않는 제품 확인
+        if diff: # 다시 꽂힐 일이 없는 제품이 있을 경우 (제품 번호는 0은 없기 때문에 그냥 이렇게 적어도 괜찮음)
+            diff = diff[0]
+            Power_socket_list.remove(diff) # 이미 꽂혀있는 제품 중 가장 마지막에 사용하는 제품 제거
             Power_socket_list.append(Product) # 새로운 제품 꽂기
             Result += 1
         else: # 다시 꽂힐 일이 있는 제품의 경우
