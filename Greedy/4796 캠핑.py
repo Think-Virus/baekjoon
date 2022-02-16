64a7abec936238846e87cb521461bdf9aa8df113
think-virus <chuncccc1004@gmail.com>
2022-02-16 20:34:07 +0900

    #4796 캠핑

--- Greedy/4796 캠핑.py
+++ Greedy/4796 캠핑.py
@@ -12,6 +12,14 @@ Case 1: 14
 Case 2: 11
 """
 
+# 틀린 것에 대한 반례 :
+"""
+2 8 20
+0 0 0
+answer : 6
+my answer : 8
+"""
+
 import sys
 
 Case_list = []
@@ -24,4 +32,13 @@ while True :
 Cnt = 0
 for _case in Case_list :
     Cnt += 1
-    print("Case {}: {}".format(Cnt,_case[2]-_case[1]*(_case[2]//_case[1])+_case[0]*(_case[2]//_case[1])))
+    L = _case[0]
+    P = _case[1]
+    V = _case[2]
+
+    if V - P*(V//P) >= L : #남은 일수보다 사용 가능한 기간이 작을 때
+        print("Case {}: {}".format(Cnt, L * (V // P) + L))
+    else :
+        print("Case {}: {}".format(Cnt, V - P * (V // P) + L * (V // P)))
+
+
