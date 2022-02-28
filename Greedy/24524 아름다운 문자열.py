2299c7cf37ca80cdc1e214fa3de6d6801d9d7d5c
think-virus <chuncccc1004@gmail.com>
2022-02-28 23:17:50 +0900

    #24524 아름다운 문자열 -> 틀림

--- Greedy/24524 아름다운 문자열.py
+++ Greedy/24524 아름다운 문자열.py
@@ -18,28 +18,24 @@ ab
 Out :
 1
 """
-from collections import defaultdict
-
-S_dict = defaultdict(list)
-for i,s in enumerate(input()) :
-    S_dict[s].append(i)
+S = list(input())
 T = input()
 
 var_check = True
 anser = 0
-
-while var_check and S_dict :
+while var_check and S :
     pre_idx = -1
     for t in T :
-        if not S_dict.get(t) :
+        try :
+            now_idx = S.index(t)
+        except ValueError :
             var_check = False
             break
-        now_idx = S_dict.get(t).pop(0)
-        if now_idx < pre_idx :
+        if pre_idx > now_idx :
             var_check = False
             break
-        else:
-            pre_idx = now_idx
+        pre_idx = now_idx
+        del S[now_idx]
     if var_check :
         anser += 1
 print(anser)
