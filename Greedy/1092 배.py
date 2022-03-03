6675dd6989eb5678d6104f9ff3f007099ef6bf50
think-virus <chuncccc1004@gmail.com>
2022-03-03 22:06:40 +0900

    1092 배 -> 시간 초과

--- Greedy/1092 배.py
+++ Greedy/1092 배.py
@@ -1,13 +1,26 @@
 # 항구에는 크레인이 N대 있고, 1분에 박스를 하나씩 배에 실을 수 있다. 모든 크레인은 동시에 움직인다!
 # 각 크레인은 무게 제한이 있다. 이 무게 제한보다 무거운 박스는 크레인으로 움직일 수 없다. 모든 박스를 배로 옮기는데 드는 시간의 최솟값을 구하는 프로그램을 작성
+
+
+"""
+원래 로직 반례
+3
+6 8 9
+9
+1 2 3 4 5 6 7 8 9
+-> 내림차순으로 도전
+"""
 import sys
-from collections import deque
+import heapq
 
 N = int(sys.stdin.readline())
 Crane_list = list(map(int, sys.stdin.readline().split()))
 
+def Box_minus(val) :
+    return int(val)*-1
+
 M = int(sys.stdin.readline())
-Box_list = list(map(int, sys.stdin.readline().split()))
+Box_list = list(map(Box_minus, sys.stdin.readline().split()))
 
 while True :
     if max(Crane_list) < min(Box_list) :
@@ -15,20 +28,31 @@ while True :
         break
 
     # 오름차순 정렬
-    Crane_list = (sorted(Crane_list))
-    Box_list = deque(sorted(Box_list))
+    Crane_list.sort(reverse=True)
+    heapq.heapify(Box_list) # -값으로 오름차순됨
+
     # 동시에 움직일 수 있는 것 체크해야 함!
     # 틀린 이유로 추측되는 거는 가장 센 애로만 들 수 있는 애가 있는 거야 그러면 못드는 게 맞을듯.
-
+    var_repeat = True
     anser = 0
-    var_continue = True
-    while Box_list and var_continue:
-        for Crane in Crane_list:
-            Box = Box_list.popleft()
+    tmp_list = []
+    while True:
+        Box_list = Box_list + tmp_list
+        if not Box_list :
+            break
+        heapq.heapify(Box_list)
+        tmp_list = []
+        for Crane in Crane_list :
             if not Box_list :
                 break
-            if Crane < Box:
-                Box_list.appendleft(Box)
+            Box = heapq.heappop(Box_list)
+            while True :
+                if Crane < -1*Box and Box_list :
+                    tmp_list.append(Box)
+                    Box = heapq.heappop(Box_list)
+                else:
+                    break
         anser += 1
+
     print(anser)
     break
