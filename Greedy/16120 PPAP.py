4e3b7e7700299c6554a4fc55791fc28a806d2380
think-virus <chuncccc1004@gmail.com>
2022-03-23 12:48:11 +0900

    #16120 PPAP -> 시간초과

--- Greedy/16120 PPAP.py
+++ Greedy/16120 PPAP.py
@@ -6,16 +6,27 @@
 # 예를 들어 PPAP는 PPAP 문자열이다. 또한, PPAP의 두 번째 P를 PPAP로 바꾼 PPPAPAP 역시 PPAP 문자열이다.
 
 # 문자열이 주어졌을 때, 이 문자열이 PPAP 문자열인지 아닌지를 알려주는 프로그램을 작성하여라.
-p = input()
 
-while p.find('A') != -1 and p.find('PPAP') != -1 : # A가 있으면 그를 PPAP -> P 로 수정해보고 마지막에 확인..?
-    s_idx = p.find('PPAP')
-    p = p[:s_idx] + 'P' + p[s_idx+4:]
+# tip을 봤을 때, 스택을 이용해서 A와 만날 때 확인하라고 하는데..
+from collections import deque
+p = deque(input())
 
-if p.find('A') != -1 :
-    if p.find('PPAP') != -1 :
-        print("PPAP")
+tmp_list = []
+
+result = []
+while p :
+    pp = p.popleft()
+    if pp == 'A' :
+        pf = p.popleft()
+        if pf == 'P' :
+            if result[-2:] == ['P','P'] :
+                result = result[:-1]
+            else:
+                print('NP')
+                exit()
+        else:
+            print('NP')
+            exit()
     else:
-        print("NP")
-else:
-    print("PPAP")
+        result.append(pp)
+print("PPAP")
