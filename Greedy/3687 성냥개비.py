e41cc146cb3b6192db04c61af05465dc18a195a7
think-virus <chuncccc1004@gmail.com>
2022-04-11 22:02:57 +0900

    #3687 성냥개비 -> 틀림

--- Greedy/3687 성냥개비.py
+++ Greedy/3687 성냥개비.py
@@ -33,31 +33,61 @@ Out :
 108 7111111
 
 큰 값을 만들 때는 첫 글자로 7이 가장 효율이 좋고 나머지는 1로 채우는 것이 좋음, 만약 1로 하기 전에 남는다면 그 값이 7보다 클 경우 4보다 앞에, 아니면 7보다 뒤로 설정!
-작은 값은 가장 많은 성냥(8)을 이용하는 것이 좋을듯
+작은 값은 가장 많은 성냥(8)과 0을 이용하는 것이 좋을듯
+
+어차피 모든 숫자는 2와 3으로 만들 수 있음..
+가장 작은 숫자를 만들 때는 8 -> 7이 좋고
+가장 큰 숫자를 만들 떄는 1 -> 2이 좋음
 """
+
+"""
+check_2 = amount // 2 # 2의 개수
+8은 성냥 7개 -> 즉 check_2가 3개 +1개가 1개는 있어야 함
+만약 개수를 충족하지 않는다면?
+다음 우선선위는 첫 자리가 아니라면 0, 첫 자리라면 6 -> check_2가 3개 필요
+다음은 2 -> check_2가 2개 +1개 필요    
+"""
+
 import sys
 
-case_dict = {2:[1],3:[7],4:[4],5:[2,3,5],6:[0,6],7:[8]}
-
+n_l = []
 for _ in range(int(sys.stdin.readline())) :
-    amount = int(sys.stdin.readline())
-    if amount == 2 :
-        print(case_dict[2][0],case_dict[2][0])
-    elif amount == 3 :
-        print(case_dict[3][0], case_dict[3][0])
-    else :
-        if amount in case_dict.keys() : # 최소값 찾기
-            if amount == 6 :
-                minval = 6
+    n_l.append(int(sys.stdin.readline()))
+
+for i in n_l :
+    min_amount = i
+    max_amount = min_amount
+
+    # 최소값 구하기
+    min_val = ''
+    while min_amount > 0 :
+        if min_amount >= 7 and (min_amount % 7 == 0 or min_amount // 7 > 1 or (min_amount // 7 > 0 and min_amount % 7 > 1)) :
+             min_val = '8'+min_val
+             min_amount -= 7
+        elif min_amount >= 6 :
+            min_amount -= 6
+            if min_amount > 0 :
+                min_val = '0'+min_val
             else:
-                minval = case_dict[amount][0]
+                min_val = '6'+min_val
+        elif min_amount >= 5 :
+            min_amount -= 5
+            min_val = '2'+min_val
+        elif min_amount >= 4 :
+            min_amount -= 4
+            min_val = '4' + min_val
+        elif min_amount >= 3:
+            min_amount -= 3
+            min_val = '7' + min_val
+        else :
+            min_amount -= 2
+            min_val = '1' + min_val
 
-        # 최댓값 찾기
-        val_7 = amount // 3
-        if (amount % 3) % 2 == 0 : # 끝까지 1인 경우
-            val_1 = (amount % 3) // 2
-        else:
-            val_1 = 0
+    # 최대값 찾기
+    check_2 = max_amount // 2
+    if max_amount % 2 == 0 :
+        max_val = '1' * check_2
+    else:
+        max_val = '7'*(max_amount % 2) + '1' * (check_2-1)
 
-        max_val = str(val_7)*7 + str(val_1)*1
-        print(minval,max_val)
+    print(min_val,max_val)
