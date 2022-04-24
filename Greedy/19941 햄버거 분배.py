57e23fcd9a8c8c14b123a08e97dd9f1b9a13034e
think-virus <chuncccc1004@gmail.com>
2022-04-24 19:51:34 +0900

    #19941 햄버거 분배

--- Greedy/19941 햄버거 분배.py
+++ Greedy/19941 햄버거 분배.py
@@ -4,25 +4,35 @@
 # 햄버거를 먹을 수 있는 사람의 최대 수를 구하는 프로그램을 작성하시오.
 
 """
-앞에서부터  순서대로 ㄱㄱ
+In
+20 1
+HHPHPPHHPPHPPPHPHPHP
+
+Out
+8
 """
 
 N,K = map(int,input().split())
 N_str = input()
-hamberger_position = []
-person_position = []
+hamberger_position = [0 for i in range(N)]
+person_position = [0 for i in range(N)]
 
 ans = 0
 for i,n in enumerate(N_str) :
     if n == 'H' :
-        hamberger_position.append(i)
+        hamberger_position[i] = 1
     else :
-        person_position.append(i)
+        person_position[i] = 1
 
-for i in person_position :
-    for j in hamberger_position :
-        if abs(i - j) <= K :
-            del hamberger_position[hamberger_position.index(j)]
+ans = 0
+for i in range(N) :
+    if person_position[i] == 1 :
+        if i-K < 0 :
+            start_K = 0
+        else :
+            start_K = i-K
+        if 1 in hamberger_position[start_K : i+K+1] :
             ans += 1
-            break            
+            hamberger_position[start_K+hamberger_position[start_K : i+K+1].index(1)] = 0
+
 print(ans)
