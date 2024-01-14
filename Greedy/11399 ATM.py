33b51af068eb4b940803084b550779e6649a901b
think_virus <chuncccc1004@gmail.com>
2024-01-14 11:18:22 +0900

    #11399 ATM -> 다시 품 (코드 재활)

--- Greedy/11399 ATM.py
+++ Greedy/11399 ATM.py
@@ -5,7 +5,22 @@ time_list = list(map(int, time_str.split(" ")))
 time_list.sort()
 sum_time = 0
 
-for i in range(usr) :
-    sum_time = sum_time + sum(time_list[:i+1])
+for i in range(usr):
+    sum_time = sum_time + sum(time_list[:i + 1])
 
 print(sum_time)
+
+# 코드 감 살리기
+N = int(input(""))
+Pi = input("")
+S = 0
+
+# Pi = list(map(int, sorted(Pi.split(" "))))
+# 이유는 모르겠지만, 백준에서 sorted는 틀림으로 책점됨
+Pi = list(map(int, Pi.split(" ")))
+Pi.sort()
+
+for i in range(N):
+    S = S + sum(Pi[:i + 1])
+
+print(S)
