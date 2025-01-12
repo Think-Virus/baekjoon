a0163dc84a50c117645a0fa6cb2ed17204a87e87
think_virus <chuncccc1004@gmail.com>
2025-01-12 18:56:00 -0500

    Stop:11050_이항계수_2 페르마 소정리에 대해 알아야 풀 수 있음.. 우선 순위 이슈로 뒤로 제외

--- 2024-/Algo_Math/11401_이항계수_3.py
+++ 2024-/Algo_Math/11401_이항계수_3.py
@@ -10,21 +10,6 @@ def input_data():
 
 def solve():
     n, k = input_data()
-    dp = [1, 1]
-
-    def combination(n, k):
-        result = factorial(n) / (factorial(n - k) * factorial(k))
-        return int(result)
-
-    def factorial(n: int) -> int:
-        if n > len(dp):
-            for i in range(len(dp), n + 1):
-                dp.append(i*dp[i-1])
-        return dp[n]
-
-    combi = combination(n, k)
-    result = combi % 1000000007
-    print(result)
 
 
 if __name__ == '__main__':
