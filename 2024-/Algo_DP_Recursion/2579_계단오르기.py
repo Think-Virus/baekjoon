aa2140e59b6d490db8842ae7fa1ee8ca989104c3
think_virus <chuncccc1004@gmail.com>
2025-02-08 14:54:19 -0600

    Fail:2579_계단오르기->틀림

---
+++ 2024-/Algo_DP_Recursion/2579_계단오르기.py
@@ -0,0 +1,96 @@
+"""
+문제
+    계단 오르기 게임은 계단 아래 시작점부터 계단 꼭대기에 위치한 도착점까지 가는 게임이다.
+    <그림 1>과 같이 각각의 계단에는 일정한 점수가 쓰여 있는데 계단을 밟으면 그 계단에 쓰여 있는 점수를 얻게 된다.
+    예를 들어 <그림 2>와 같이 시작점에서부터 첫 번째, 두 번째, 네 번째, 여섯 번째 계단을 밟아 도착점에 도달하면 총 점수는 10 + 20 + 25 + 20 = 75점이 된다.
+    계단 오르는 데는 다음과 같은 규칙이 있다.
+        계단은 한 번에 한 계단씩 또는 두 계단씩 오를 수 있다. 즉, 한 계단을 밟으면서 이어서 다음 계단이나, 다음 다음 계단으로 오를 수 있다.
+        연속된 세 개의 계단을 모두 밟아서는 안 된다. 단, 시작점은 계단에 포함되지 않는다.
+        마지막 도착 계단은 반드시 밟아야 한다.
+    따라서 첫 번째 계단을 밟고 이어 두 번째 계단이나, 세 번째 계단으로 오를 수 있다.
+    하지만, 첫 번째 계단을 밟고 이어 네 번째 계단으로 올라가거나, 첫 번째, 두 번째, 세 번째 계단을 연속해서 모두 밟을 수는 없다.
+    각 계단에 쓰여 있는 점수가 주어질 때 이 게임에서 얻을 수 있는 총 점수의 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+    입력의 첫째 줄에 계단의 개수가 주어진다.
+    둘째 줄부터 한 줄에 하나씩 제일 아래에 놓인 계단부터 순서대로 각 계단에 쓰여 있는 점수가 주어진다.
+    계단의 개수는 300이하의 자연수이고, 계단에 쓰여 있는 점수는 10,000이하의 자연수이다.
+
+출력
+    첫째 줄에 계단 오르기 게임에서 얻을 수 있는 총 점수의 최댓값을 출력한다.
+
+6
+10
+20
+15
+25
+10
+20
+
+75
+---
+접근법:
+    step[i] : ith 점수
+    dp[i] : i까지 얻을 수 있는 최대 점수
+
+    dp[0] = 0
+    dp[1] = step[1]
+    dp[2] = step[1] + step[2]
+    dp[3] = max(step[1] + step[2], step[2] + step[3], step[1] + step[3])
+    case 1) Go ith
+        1.1) Don't go i-1th
+            i : O
+            i-1 : X
+            i-2 : O~
+                step[i] + dp[i-2]
+        1.2) Go i-1th
+            i : O
+            i-1 : O
+            i-2 : X
+            i-3 : O~
+                step[i] + step[i-1] + dp[i-3]
+    case 2) Don't go ith
+        i : X
+        i - 1 : O~
+            dp[i] = dp[i-1]
+
+    dp[i] =
+        max(step[i] + dp[i-2], step[i] + step[i-1] + dp[i-3], dp[i-1]) if i != n
+        max(step[i] + dp[i-2], step[i] + step[i-1] + dp[i-3]) if i == n
+"""
+import sys
+
+
+def input_data() -> (int, list[int]):
+    n = int(sys.stdin.readline())
+    steps = [0] + [int(sys.stdin.readline()) for s in range(n)]
+
+    return n, steps
+
+
+def solve():
+    n, steps = input_data()
+
+    def sol_dp(n, steps):
+        dp = [0] * (n + 1)
+        dp[1] = steps[1]
+        dp_third = 0
+        if n <= 2:
+            print(sum(steps))
+            return
+        dp_second = steps[1] + steps[2]
+        if n >= 3:
+            dp_third = max(steps[1] + steps[2], steps[2] + steps[3], steps[1] + steps[3])
+        dp[2] = dp_second
+        dp[3] = dp_third
+
+        for i in range(4, n + 1):
+            dp[i] = max(steps[i] + dp[i - 2], steps[i] + steps[i - 1] + dp[i - 3], dp[i - 1]) if i != n \
+                else max(steps[i] + dp[i - 2], steps[i] + steps[i - 1] + dp[i - 3])
+
+        print(dp[n])
+
+    sol_dp(n, steps)
+
+if __name__ == '__main__':
+    solve()
