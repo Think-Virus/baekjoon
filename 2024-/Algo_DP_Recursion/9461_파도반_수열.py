f7eacdc3f7c3bd35c3125cce9c039a17824f4b8c
think_virus <chuncccc1004@gmail.com>
2025-02-23 12:51:56 -0600

    Success:9461_파도반_수열

---
+++ 2024-/Algo_DP_Recursion/9461_파도반_수열.py
@@ -0,0 +1,44 @@
+"""
+문제
+    오른쪽 그림과 같이 삼각형이 나선 모양으로 놓여져 있다. 첫 삼각형은 정삼각형으로 변의 길이는 1이다.
+    그 다음에는 다음과 같은 과정으로 정삼각형을 계속 추가한다.
+    나선에서 가장 긴 변의 길이를 k라 했을 때, 그 변에 길이가 k인 정삼각형을 추가한다.
+    파도반 수열 P(N)은 나선에 있는 정삼각형의 변의 길이이다. P(1)부터 P(10)까지 첫 10개 숫자는 1, 1, 1, 2, 2, 3, 4, 5, 7, 9이다.
+    N이 주어졌을 때, P(N)을 구하는 프로그램을 작성하시오.
+
+입력
+    첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. (1 ≤ N ≤ 100)
+
+출력
+    각 테스트 케이스마다 P(N)을 출력한다.
+---
+    dp[i] = dp[i-2] + dp[i-3]
+"""
+
+
+def input_data():
+    t = int(input())
+    test_data = [int(input()) for _ in range(t)]
+
+    return test_data
+
+
+def solve(test_data):
+    dp = [0 for _ in range(101)]
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    last_i = 3
+
+    for i in test_data:
+        if last_i >= i:
+            print(dp[i])
+        else:
+            for j in range(last_i + 1, i + 1):
+                dp[j] = dp[j - 2] + dp[j - 3]
+            print(dp[i])
+
+
+if __name__ == '__main__':
+    test_data = input_data()
+    solve(test_data)
