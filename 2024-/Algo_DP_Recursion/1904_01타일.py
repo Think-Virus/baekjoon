f1ac11911822bc92d5209b110de5e8d2a9d6b87f
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-03-16 16:12:00 -0500

    Fail: 1904_01타일 -> IndexError

--- 2024-/Algo_DP_Recursion/1904_01타일.py
+++ 2024-/Algo_DP_Recursion/1904_01타일.py
@@ -1,14 +1,13 @@
 def solve():
     n = int(input())
-    dp1 = 1
-    dp2 = 2
-    dp3 = dp1 + dp2
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
 
-    for i in range(4, n + 1):
-        dp1 = dp2
-        dp2 = dp3
-        dp3 = dp1 + dp2
-    print(dp3 % 15746)
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+    print(dp[n])
 
 
 if __name__ == '__main__':
