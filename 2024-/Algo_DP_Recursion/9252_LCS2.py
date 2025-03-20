dec1295b41e124217ed6bc9bb9e172c8b9d20135
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-03-20 11:21:00 -0500

    Fail: 9252_LCS2 -> IndexError

---
+++ 2024-/Algo_DP_Recursion/9252_LCS2.py
@@ -0,0 +1,23 @@
+def solve():
+    first = input()
+    second = input()
+    length = len(first)
+
+    dp = [[0] * (length + 1) for _ in range(length + 1)]
+
+    result_str = ""
+    for i in range(1, length + 1):
+        for j in range(1, length + 1):
+            if first[i - 1] == second[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > len(result_str):
+                    result_str += first[i - 1]
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+    print(dp[length][length])
+    print(result_str)
+
+
+if __name__ == '__main__':
+    solve()
