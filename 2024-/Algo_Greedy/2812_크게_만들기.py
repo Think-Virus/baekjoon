b82164822e60529e87dbb729f337243b340176a9
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-03-30 19:37:00 -0500

    Fail: 2812_크게_만들기 -> Mistake

--- 2024-/Algo_Greedy/2812_크게_만들기.py
+++ 2024-/Algo_Greedy/2812_크게_만들기.py
@@ -5,12 +5,27 @@ def solve():
     n, k = map(int, input().split())
     total_number = input()
     number_order = []
+    result = [''] * n
+    cnt = 0
 
     for i, number in enumerate(total_number):
-        heapq.heappush(number_order, (int(number), i))
+        heapq.heappush(number_order, (-1 * int(number), i))
 
-    for _ in range(k):
-        print(heapq.heappop(number_order))
+    while cnt != n - k:
+        passed_list = []
+        largest_num_index = number_order[0][1]
+        while number_order:
+            curr_num, curr_i = heapq.heappop(number_order)
+            if curr_i >= largest_num_index:
+                result[curr_i] = -1 * curr_num
+                cnt += 1
+
+                if cnt == n - k:
+                    break
+            else:
+                heapq.heappush(passed_list, (int(curr_num), curr_i))
+        number_order = passed_list
+    print(*result, sep='')
 
 
 if __name__ == '__main__':
