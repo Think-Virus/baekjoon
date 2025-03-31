8326ba6c675495fe9767f55f9ee526bfc1075f67
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-03-30 21:17:00 -0500

    Fail: 2812_크게_만들기 -> Mistake

--- 2024-/Algo_Greedy/2812_크게_만들기.py
+++ 2024-/Algo_Greedy/2812_크게_만들기.py
@@ -11,8 +11,18 @@ def solve():
     for i, number in enumerate(total_number):
         heapq.heappush(number_order, (-1 * int(number), i))
 
+    start_index = number_order[0][1] + 1
+    end_index = n - 1
+    is_next = True
+
     while cnt != n - k:
         curr_num, curr_i = heapq.heappop(number_order)
+
+        if result[curr_i]:
+            continue
+
+        if curr_i < start_index and not is_next:
+            continue
 
         result[curr_i] = -1 * curr_num
         cnt += 1
@@ -20,22 +30,36 @@ def solve():
         if cnt == n - k:
             break
 
-        tmp_heap = []
-        while number_order:
-            tmp_num, tmp_i = heapq.heappop(number_order)
-            if tmp_i < curr_i:
-                heapq.heappush(tmp_heap, (tmp_num, tmp_i))
-            else:
-                result[tmp_i] = -1 * tmp_num
+        start_index = curr_i + 1
+        tmp_heapq = []
+        if n - k - cnt >= end_index - start_index + 1:
+            for i in range(start_index, end_index + 1):
+                result[i] = total_number[i]
                 cnt += 1
-                curr_i = tmp_i
+
                 if cnt == n - k:
                     break
-        number_order = tmp_heap
+            end_index = curr_i - 1
+            is_next = True
+        else:
+            for i in range(start_index, end_index + 1):
+                heapq.heappush(tmp_heapq, (-1 * int(total_number[i]), i))
+
+            while tmp_heapq:
+                tmp_num, tmp_i = heapq.heappop(tmp_heapq)
+                if tmp_i < start_index:
+                    continue
+
+                result[tmp_i] = -1 * tmp_num
+                cnt += 1
+                if cnt == n - k:
+                    break
+                start_index = tmp_i + 1
+            is_next = False
+        start_index = curr_i + 1
 
     print(*result, sep='')
 
 
 if __name__ == '__main__':
-    while True:
-        solve()
+    solve()
