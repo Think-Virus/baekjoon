9c7e6a56b75e12242c8018aa217f25574ae05423
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-03-30 19:51:00 -0500

    Proceeding: 2812_크게_만들기 -> second idea was wrong

--- 2024-/Algo_Greedy/2812_크게_만들기.py
+++ 2024-/Algo_Greedy/2812_크게_만들기.py
@@ -11,20 +11,20 @@ def solve():
     for i, number in enumerate(total_number):
         heapq.heappush(number_order, (-1 * int(number), i))
 
+    largest_num_index = number_order[0][1]
+    left = n - largest_num_index
     while cnt != n - k:
-        passed_list = []
-        largest_num_index = number_order[0][1]
-        while number_order:
-            curr_num, curr_i = heapq.heappop(number_order)
-            if curr_i >= largest_num_index:
+        curr_num, curr_i = heapq.heappop(number_order)
+        if curr_i >= largest_num_index:
+            result[curr_i] = -1 * curr_num
+            cnt += 1
+            left -= 1
+        else:
+            if n - k - cnt > left:
+                largest_num_index = curr_i
                 result[curr_i] = -1 * curr_num
                 cnt += 1
-
-                if cnt == n - k:
-                    break
-            else:
-                heapq.heappush(passed_list, (int(curr_num), curr_i))
-        number_order = passed_list
+                left = n - curr_i - cnt
     print(*result, sep='')
 
 
