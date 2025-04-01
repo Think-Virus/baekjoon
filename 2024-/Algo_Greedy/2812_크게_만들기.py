6ca4c85bb0571d66c0b017410f0c7e91b261595c
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-04-01 17:20:00 -0500

    Success: 2812_크게_만들기 -> Solved after referring to the solution

--- 2024-/Algo_Greedy/2812_크게_만들기.py
+++ 2024-/Algo_Greedy/2812_크게_만들기.py
@@ -1,43 +1,18 @@
-import heapq
-
-
 def solve():
     n, k = map(int, input().split())
     total_number = input()
+    stack = []
 
-    result = [''] * n
+    for number in total_number:
+        while stack and stack[-1] < number and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(number)
 
-    def fill_result(s_index, f_index, rest):
-        if f_index - s_index < 0:
-            return None
-
-        if f_index - s_index < rest:
-            result[s_index:f_index + 1] = total_number[s_index:f_index + 1]
-            return result, s_index - 1, rest - (f_index - s_index)
-
-        while rest != 0:
-            number_order = []
-            for i, number in enumerate(total_number[s_index:f_index + 1]):
-                heapq.heappush(number_order, (-1 * int(number), i + s_index))
-            rest -= 1
-
-            curr_max_num, curr_i = heapq.heappop(number_order)
-            result[curr_i] = -1 * curr_max_num
-            if rest == 0:
-                return result, curr_i, rest
-
-            fill_result_set = fill_result(curr_i + 1, f_index, rest)
-            if fill_result_set:
-                result[curr_i + 1:f_index + 1] = fill_result_set[0][curr_i + 1:f_index + 1]
-                f_index = fill_result_set[1]
-                rest = fill_result_set[2]
-            else:
-                f_index -= 1
-
-        return result, f_index, rest
-
-    fill_result(0, n - 1, n - k)[0]
-    print(*result, sep='')
+    if k > 0:
+        print(''.join(stack[:-k]))
+    else:
+        print(''.join(stack))
 
 
 if __name__ == '__main__':
