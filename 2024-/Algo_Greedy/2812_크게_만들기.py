9dac617e1ca5b699324b30ccb688337d1df017fb
Myeong-Gyun, Choi <chuncccc1004@gmail.com>
2025-04-01 15:10:00 -0500

    Fail: 2812_크게_만들기 -> MLO

--- 2024-/Algo_Greedy/2812_크게_만들기.py
+++ 2024-/Algo_Greedy/2812_크게_만들기.py
@@ -7,19 +7,18 @@ def solve():
 
     def fill_result(s_index, f_index, rest):
         if f_index - s_index < 0:
-            print(0)
             return None
 
-        r_result = [''] * n
+        r_result = [''] * (f_index - s_index + 1)
 
         if f_index - s_index < rest:
-            r_result[s_index:f_index + 1] = total_number[s_index:f_index + 1]
+            r_result = total_number[s_index:f_index + 1]
             return r_result, s_index - 1, rest - (f_index - s_index)
 
         while rest != 0:
             number_order = []
             for i, number in enumerate(total_number[s_index:f_index + 1]):
-                heapq.heappush(number_order, (-1 * int(number), i + s_index))
+                heapq.heappush(number_order, (-1 * int(number), i))
             rest -= 1
 
             curr_max_num, curr_i = heapq.heappop(number_order)
@@ -27,9 +26,9 @@ def solve():
             if rest == 0:
                 return r_result, curr_i, rest
 
-            fill_result_set = fill_result(curr_i + 1, f_index, rest)
+            fill_result_set = fill_result(s_index + curr_i + 1, f_index, rest)
             if fill_result_set:
-                r_result[curr_i + 1:f_index + 1] = fill_result_set[0][curr_i + 1:f_index + 1]
+                r_result[curr_i + 1:f_index + 1] = fill_result_set[0]
                 f_index = fill_result_set[1]
                 rest = fill_result_set[2]
             else:
